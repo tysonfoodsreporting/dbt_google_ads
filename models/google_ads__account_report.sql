d23a2395409db4c69cdb6234806b6377ad69455a
Anuya Brahme <anuyab@cybage.com>
2026-03-11 14:50:41 +0530

    changes at account level

--- models/google_ads__account_report.sql
+++ models/google_ads__account_report.sql
@@ -7,44 +7,47 @@
     }
     ) }}
 
-with stats as (
-
+with campaign_stats as (
+ 
     select *
-    from {{ var('account_stats') }}
-), 
-
+    from {{ var('campaign_stats') }}
+    where lower(campaign_name) not like '%yt-dg%'
+ 
+),
+ 
 accounts as (
-
+ 
     select *
     from {{ var('account_history') }}
     where is_most_recent_record = True
-), 
-
+ 
+),
+ 
 fields as (
-
+ 
     select
-        stats.source_relation,
-        stats.date_day,
+        campaign_stats.source_relation,
+        campaign_stats.date_day,
         accounts.account_name,
-        stats.account_id,
+        campaign_stats.account_id,
         accounts.currency_code,
         accounts.auto_tagging_enabled,
         accounts.time_zone,
-        sum(stats.spend) as spend,
-        sum(stats.clicks) as clicks,
-        sum(stats.impressions) as impressions,
-        sum(stats.conversions) as conversions,
-        sum(stats.conversions_value) as conversions_value,
-        sum(stats.view_through_conversions) as view_through_conversions
-
-        {{ google_ads_persist_pass_through_columns(pass_through_variable='google_ads__account_stats_passthrough_metrics', identifier='stats', transform='sum', coalesce_with=0, exclude_fields=['conversions','conversions_value','view_through_conversions']) }}
-
-    from stats
+        sum(campaign_stats.spend) as spend,
+        sum(campaign_stats.clicks) as clicks,
+        sum(campaign_stats.impressions) as impressions,
+        sum(campaign_stats.conversions) as conversions,
+        sum(campaign_stats.conversions_value) as conversions_value,
+        sum(campaign_stats.view_through_conversions) as view_through_conversions
+ 
+    from campaign_stats
     left join accounts
-        on stats.account_id = accounts.account_id
-        and stats.source_relation = accounts.source_relation
+        on campaign_stats.account_id = accounts.account_id
+        and campaign_stats.source_relation = accounts.source_relation
+ 
     {{ dbt_utils.group_by(7) }}
+ 
 )
-
+ 
 select *
 from fields
